31d2268e115548f475422ca5cc0fafe0b65e56da
Py is Py <62064902+xVoldx@users.noreply.github.com>
2021-06-25 11:39:39 +0100

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
 {
-sudo useradd -m blackfire
-sudo adduser blackfire sudo
-echo 'blackfire:8426' | sudo chpasswd
+sudo useradd -m RCA
+sudo adduser RCA sudo
+echo 'RCA:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,13 +18,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser blackfire chrome-remote-desktop
+sudo adduser RCA chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - blackfire -c """$CRP"""
+su - RCA -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
